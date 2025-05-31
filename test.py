17981f66c86b319d425144cf638f4f3dcd303c7b
Blaukreutz <100523449+Blaukreutz@users.noreply.github.com>
2025-05-31 15:43:18 -0400

    stuff

--- test.py
+++ test.py
@@ -1,13 +1,13 @@
 import cv2
 from Image_Processing import *
 
-cv2.namedWindow("preview")
+#cv2.namedWindow("preview")
 cam = cv2.VideoCapture(0)
 
 while True:
     #process camera inputs
     ret, frame = cam.read()
-    cv2.imshow("preview", frame)
+    #cv2.imshow("preview", frame)
     data = get_target_data(find_largest_ellipse(frame))
     if data is not None:
         print(data)
@@ -15,5 +15,5 @@ while True:
     if key == 27: # exit on ESC
         break
 
-cv2.destroyWindow("preview")
+#cv2.destroyWindow("preview")
 cam.release()
